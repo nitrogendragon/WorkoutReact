a1837ef9fca97a0bef32b868b7b2a836d86081a5
nitrogendragon <dirtboy4fun@gmail.com>
2020-02-27 22:40:38 -0600

    tiny style fix

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -3,7 +3,7 @@ import '../styles/exercises.css'
 export default function TimerComponent(props) {
     return (
         <div>{props.timerRunning ? 
-            <><p> className = "p-bold"TimeRemaining</p>
+            <><p className = "p-bold"> TimeRemaining</p>
             <p className = "p-bold">{props.timeRemaining}</p></> :
             <p className = "p-bold">Time Left Will show up here</p>
         }
